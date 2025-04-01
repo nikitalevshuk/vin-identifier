dea342b016e079b38c85d7a8f31fb70c8ae60336
nikitalevshuk <levshukn@gmail.com>
2025-04-01 17:33:53 +0300

    made config

---
+++ app/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    QWEN_API_KEY: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
